5a7ecc8e907125c8eada67b127f0d31a5d5aa919
DavidOfficialol <110808196+DavidOfficialol@users.noreply.github.com>
2022-12-08 14:22:34 +1030

    Update DiceKit.swift

--- DiceKit/DiceKit.swift
+++ DiceKit/DiceKit.swift
@@ -1,2 +1,14 @@
-import SwiftUI
+public final class DiceKit {
+    public init() {}
+    
+    public func sixsidedie() -> Int {
+        return Int.random(in: 1...6)
+    }
+    public func foursidedie() -> Int {
+        return Int.random(in: 1...4)
+    }
+    
+}
+
+
 
